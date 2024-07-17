e8b93e85e17dfb4c111b28a0c71536c4bbe881a3
gilaguilar <redeyedev.208@gmail.com>
2024-07-16 21:22:07 -0600

    set main view layout, working on color scheme next

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,11 +1,30 @@
 import { Button } from '@/components/ui/button';
+import { PersonStandingIcon } from 'lucide-react';
+import Link from 'next/link';
 
 export default function LandingPage() {
   return (
-    <div>
-      <h1>Support Me</h1>
-      <Button>Log in</Button>
-      <Button>Sign up</Button>
-    </div>
+    <>
+      <h1 className='flex gap-2 items-center'>
+        <PersonStandingIcon
+          size={50}
+          className='text-purple-500'
+        />
+        Support Dashboard
+      </h1>
+      <p>A dashboard to manage customer support</p>
+      <div className='flex gap-2 items-center'>
+        <Button asChild>
+          <Link href='/login'>Log in</Link>
+        </Button>
+        <small>or</small>
+        <Button
+          variant='outline'
+          asChild
+        >
+          <Link href='/sign-up'>Sign up</Link>
+        </Button>
+      </div>
+    </>
   );
 }
