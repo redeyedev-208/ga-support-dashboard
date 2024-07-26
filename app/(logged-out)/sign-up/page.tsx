50a5d803e59ffb3b7ed1540cf24ae27d97058b49
gilaguilar <redeyedev.208@gmail.com>
2024-07-25 20:56:52 -0600

    added new select component with select items

--- app/(logged-out)/sign-up/page.tsx
+++ app/(logged-out)/sign-up/page.tsx
@@ -19,6 +19,13 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { PersonStandingIcon } from 'lucide-react';
 import Link from 'next/link';
@@ -27,6 +34,9 @@ import * as z from 'zod';
 
 const formSchema = z.object({
   email: z.string().email(),
+  accountType: z.enum(['personal', 'company']),
+  companyName: z.string().optional(),
+  numberOfEmployees: z.coerce.number().optional(),
 });
 
 export default function SignupPage() {
@@ -80,6 +90,28 @@ export default function SignupPage() {
                     </FormControl>
 
                     <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='accountType'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      <Select onValueChange={field.onChange}>
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder='Select and account type' />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value='personal'>Personal</SelectItem>
+                          <SelectItem value='company'>Company</SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormLabel>
                   </FormItem>
                 )}
               />
