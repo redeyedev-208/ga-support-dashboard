5e019c41f61c469d96d4108d759d83042079501d
gilaguilar <redeyedev.208@gmail.com>
2024-09-20 12:25:55 -0600

    side panel items complete with link component

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -1,3 +1,4 @@
+import MenuItem from './menu-item';
 import MenuTitle from './menu-title';
 
 export default function MainMenu() {
@@ -5,6 +6,13 @@ export default function MainMenu() {
     <div className='bg-muted overflow-auto p-4'>
       <div className='border-b dark:border-b-black border-b-zinc-300 pb-4'>
         <MenuTitle />
+      </div>
+      <div className='py-4'>
+        <MenuItem href='/dashboard'>My dashboard</MenuItem>
+        <MenuItem href='/dashboard/teams'>Teams</MenuItem>
+        <MenuItem href='/dashboard/employees'>Employees</MenuItem>
+        <MenuItem href='/dashboard/account'>Account</MenuItem>
+        <MenuItem href='/dashboard/settings'>Settings</MenuItem>
       </div>
     </div>
   );
