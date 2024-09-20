67f91a8dff99ea23cc3ffb7ad5c7175bbcb7b4a0
gilaguilar <redeyedev.208@gmail.com>
2024-09-20 13:05:02 -0600

    added left sidebar footer with light dark toggle

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -1,18 +1,35 @@
+import Link from 'next/link';
 import MenuItem from './menu-item';
 import MenuTitle from './menu-title';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { LightDarkToggle } from '@/components/ui/light-dark-toggle';
 
 export default function MainMenu() {
   return (
-    <div className='bg-muted overflow-auto p-4'>
+    <div className='bg-muted overflow-auto p-4 flex flex-col'>
       <div className='border-b dark:border-b-black border-b-zinc-300 pb-4'>
         <MenuTitle />
       </div>
-      <div className='py-4'>
+      <div className='py-4 grow'>
         <MenuItem href='/dashboard'>My dashboard</MenuItem>
         <MenuItem href='/dashboard/teams'>Teams</MenuItem>
         <MenuItem href='/dashboard/employees'>Employees</MenuItem>
         <MenuItem href='/dashboard/account'>Account</MenuItem>
         <MenuItem href='/dashboard/settings'>Settings</MenuItem>
+      </div>
+      <div className='flex gap-2 items-center'>
+        <Avatar>
+          <AvatarFallback className='bg-blue-300 dark:bg-blue-900'>
+            GA
+          </AvatarFallback>
+        </Avatar>
+        <Link
+          className='underline'
+          href='/'
+        >
+          Logout
+        </Link>
+        <LightDarkToggle className='ml-auto' />
       </div>
     </div>
   );
--- app/dashboard/components/menu-item.tsx
+++ app/dashboard/components/menu-item.tsx
@@ -16,7 +16,7 @@ export default function MenuItem({ children, href }: Props) {
       className={cn(
         'block p-2 hover:bg-white dark:hover:bg-zinc-700 rounded-md text-muted-foreground hover:text-foreground',
         isActive &&
-          'bg-primary hover:bg-primary dark:hover:bg-primary hover:text-foreground text-foreground',
+          'bg-primary hover:bg-primary dark:hover:bg-primary hover:text-primary-foreground text-primary-foreground',
       )}
       href={href}
     >
