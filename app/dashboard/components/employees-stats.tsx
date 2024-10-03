fddadb0b66a42b11a22757db118ea6300c1b831a
gilaguilar <redeyedev.208@gmail.com>
2024-10-02 20:05:16 -0600

    added button component and styling will override this for a more specific desired size

--- app/dashboard/components/employees-stats.tsx
+++ app/dashboard/components/employees-stats.tsx
@@ -1,11 +1,40 @@
-import { Card } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { UserIcon } from 'lucide-react';
+import Link from 'next/link';
 
 export default function EmployeesStats() {
   return (
     <div className='grid lg:grid-cols-3 gap-4'>
-      <Card>Card 1</Card>
-      <Card>Card 2</Card>
-      <Card>Card 3</Card>
+      <Card>
+        <CardHeader>
+          <CardTitle className='text-base'>Total employees</CardTitle>
+        </CardHeader>
+        <CardContent className='flex justify-between items-center'>
+          <div className='flex gap-2'>
+            <UserIcon />
+            <div className='text-5xl font-bold'>100</div>
+          </div>
+          <div>
+            <Button
+              size='sm'
+              asChild
+            >
+              <Link href='/dashboard/employees'>View all</Link>
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle className='text-base'>Employees present</CardTitle>
+        </CardHeader>
+      </Card>
+      <Card className='border-purple-500'>
+        <CardHeader>
+          <CardTitle className='text-base'>Employee of the month</CardTitle>
+        </CardHeader>
+      </Card>
     </div>
   );
 }
