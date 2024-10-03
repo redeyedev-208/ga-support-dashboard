f820f2afe22c51cad335f79c9866b9922e46cfa6
gilaguilar <redeyedev.208@gmail.com>
2024-10-02 19:52:38 -0600

    added tabs component and addressed responsiveness

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,17 @@
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
+import EmployeesStats from './components/employees-stats';
+
 export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+  return (
+    <Tabs defaultValue='employees'>
+      <TabsList className='mb-4'>
+        <TabsTrigger value='employees'>Employees stats</TabsTrigger>
+        <TabsTrigger value='teams'>Teams stats</TabsTrigger>
+      </TabsList>
+      <TabsContent value='employees'>
+        <EmployeesStats />
+      </TabsContent>
+      <TabsContent value='teams'>Teams stats view</TabsContent>
+    </Tabs>
+  );
 }
