0962c61e2222058486e20256f18c78441ac6f48e
Gil Aguilar <thehopefulautistic@gmail.com>
2024-12-28 19:35:27 -0700

    feat:added skeletons and set timeout to loading state

--- app/dashboard/employees/page.tsx
+++ app/dashboard/employees/page.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import { employees } from './employees';
+import { setTimeout } from 'timers/promises';
 
-export default function EmployeesPage() {
+export default async function EmployeesPage() {
+  await setTimeout(5000);
+
   return (
     <div>
       <h2>Employees Page</h2>
