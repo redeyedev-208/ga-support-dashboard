592e5d7d58280eb5b14567196933f34827f4496c
Gil Aguilar <thehopefulautistic@gmail.com>
2024-12-28 20:08:24 -0700

    feat:added table layout for Employees section

--- app/dashboard/employees/page.tsx
+++ app/dashboard/employees/page.tsx
@@ -1,12 +1,23 @@
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { employees } from './employees';
 import { setTimeout } from 'timers/promises';
+import { DataTable } from '@/components/ui/data-table';
+import { columns } from './columns';
 
 export default async function EmployeesPage() {
   await setTimeout(5000);
 
   return (
-    <div>
-      <h2>Employees Page</h2>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Employees</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <DataTable
+          data={employees}
+          columns={columns}
+        />
+      </CardContent>
+    </Card>
   );
 }
